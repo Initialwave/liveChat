4230ebf5b3f4a5bb8bc962292101a30be8f3029a
Daniel Ratzlaff <Asylumgames39@gmail.com>
2023-12-05 16:34:45 -0600

    connected frontend to backend

--- ReactProjects/Chatapp/Server/index.js
+++ ReactProjects/Chatapp/Server/index.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const app = express();
-const http = require('http');
-const cors = require('cors');
-const { Server } = require("socket.io")
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on("connection", (socket) => {
-    console.log(socket.id);
+io.on("connection", socket => {
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on("disconnect", () => {
-        console.log("USER DISCONNECTED", socket.id)
-    });
+  socket.on("disconnect", () => {
+    console.log("USER DISCONNECTED", socket.id);
+  });
 });
 
 server.listen(3001, () => {
-    console.log("SERVER RUNNING")
-})
+  console.log("SERVER RUNNING");
+});
